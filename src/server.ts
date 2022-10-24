2ebb7e44ff05af9bc300ca168f758e4d255d899e
Victor Nunes <victornunesc95@gmail.com>
2022-10-24 10:07:02 -0300

    hotfix: added environment variable for connection with Redis on heroku

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,13 @@ app.listen(PORT, () => {
   console.info(`App is running on port ${PORT}!`);
 });
 
-const redisClient = createClient();
+const redisClient = createClient({
+  url: process.env.REDIS_URL,
+  socket: {
+    tls: true,
+    rejectUnauthorized: false,
+  },
+});
 
 redisClient.on("error", (error) => console.error(`Error : ${error}`));
 
